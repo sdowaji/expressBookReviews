5f1bc678a3b045eb9cd039252daa049a9c75bfaa
sdowaji <sdowaji@gmail.com>
2026-07-03 23:21:04 -0400

    Modified get by auther

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,7 +50,24 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  new Promise((resolve, reject) => {
+    const author = req.params.author
+    const booksByAuthor = Object.values(books).filter(
+      (b) => b.author === author
+    )
+    if (booksByAuthor.length === 0) {
+      reject('No books found for this author')
+    } else {
+      resolve(booksByAuthor)
+    }
+  })
+    .then((data) => {
+      res.status(200).json(data)
+    })
+    .catch((error) => {
+      res.status(404).json({ message: error })
+    })
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title

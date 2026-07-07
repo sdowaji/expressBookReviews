6c4bee14ec5ce9b77ffa5c9ef1df802456780454
sdowaji <sdowaji@gmail.com>
2026-07-07 12:30:22 -0400

    Register new user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,18 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Invalid username or password' })
+  }
+  if (users.includes(username)) {
+    return res.status(400).json({ message: 'User already exists' })
+  } else {
+    users.push({ username, password })
+    return res.status(200).json({ message: 'User registered successfully' })
+  }
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
